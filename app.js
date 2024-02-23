eb2569dc397ca8def39cbd99a4467bdb09f4e2c8
cauamicael <cauamicael@hotmail.com>
2024-02-23 02:35:25 -0300

    Criação de tabelas, models, rotas para adicionar registro e inserção de dados via postman

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const app = express();
-const db = require("./db/connection")
+const express    = require("express");
+const app        = express();
+const db         = require("./db/connection");
+const bodyParser = require("body-parser");
+
 
 const PORT = 3000;
 
 app.listen(PORT, function () {
-    console.log(`O express esta rodadno na porta ${PORT}`);
+    console.log(`O express esta rodando na porta ${PORT}`);
 });
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 //db connection
 db
@@ -16,9 +22,12 @@ db
     })
     .catch(err => {
         console.log("Ocorreu um erro ao conectar", err);
-    })
+    });
 
 //routes
 app.get("/", (req, res) => {
     res.send("Esta funcionando 3");
 });
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"));
